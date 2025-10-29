9f89f2ec16bb0e2ef988b1ef13bf7bc17ef52a0f
LucaRay <me@luca.video>
2025-10-29 00:30:16 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://little-sparrow.vercel.app',
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-    speedInsights: { enabled: true }
-  }),
+  output: 'static',
   integrations: [tailwind(), sitemap()],
 });
